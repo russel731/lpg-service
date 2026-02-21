4dc95799fc99864fde77a226092bdd27faa80755
russel731 <faustspb@gmail.com>
2026-02-21 20:11:19 +0300

    Update owner.py

--- backend/app/schemas/owner.py
+++ backend/app/schemas/owner.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class OwnerCreate(BaseModel):
     telegram_id: int
     phone: str
     station_name: str
-    latitude: float
-    longitude: float
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
